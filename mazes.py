382b67d8865127269065b1daa07359dbe960ae95
Nikhil Kakodkar <nikhil.kakodkar@mail.mcgill.ca>
2018-04-08 17:16:07 -0400

    Cosmetic Changes

--- mazes.py
+++ mazes.py
@@ -1,99 +1,100 @@
 # Import necessary packages
 import curses
-import time
-import sys
 
 # Pycolab
 from pycolab.prefab_parts import sprites as prefab_sprites
 from pycolab import ascii_art
 from pycolab import human_ui
 
+
 def make_maze(maze, maze_type):
-	if maze_type == 'SIMPLE_MAZE_R1':
-		"""Builds and returns a blocking maze gridworld game."""
-		return ascii_art.ascii_art_to_game(
-			maze, what_lies_beneath=' ',
-			sprites={'P': PlayerSpriteSimpleR1})
-	elif maze_type == 'SIMPLE_MAZE_R2':
-		"""Builds and returns a blocking maze gridworld game."""
-		return ascii_art.ascii_art_to_game(
-			maze, what_lies_beneath=' ',
-			sprites={'P': PlayerSpriteSimpleR2})
+    if maze_type == 'SIMPLE_MAZE_R1':
+        """Builds and returns a blocking maze gridworld game."""
+        return ascii_art.ascii_art_to_game(
+            maze, what_lies_beneath=' ',
+            sprites={'P': PlayerSpriteSimpleR1})
+    elif maze_type == 'SIMPLE_MAZE_R2':
+        """Builds and returns a blocking maze gridworld game."""
+        return ascii_art.ascii_art_to_game(
+            maze, what_lies_beneath=' ',
+            sprites={'P': PlayerSpriteSimpleR2})
+
 
 class PlayerSpriteSimpleR2(prefab_sprites.MazeWalker):
-	"""A `Sprite` for our player.
+    """A `Sprite` for our player.
 
-	This `Sprite` ties actions to going in the four cardinal directions. If we
-	reach a magical location (in this example, (4, 3)), the agent receives a
-	reward of 1 and the epsiode terminates.
-	"""
+    This `Sprite` ties actions to going in the four cardinal directions. If we
+    reach a magical location (in this example, (4, 3)), the agent receives a
+    reward of 1 and the epsiode terminates.
+    """
 
-	def __init__(self, corner, position, character):
-		"""Inform superclass that we can't walk through walls."""
-		super(PlayerSpriteSimpleR2, self).__init__(
-			corner, position, character, impassable='#')
+    def __init__(self, corner, position, character):
+        """Inform superclass that we can't walk through walls."""
+        super(PlayerSpriteSimpleR2, self).__init__(
+          corner, position, character, impassable='#')
 
-	def update(self, actions, board, layers, backdrop, things, the_plot):
-		del layers, backdrop, things   # Unused.
+    def update(self, actions, board, layers, backdrop, things, the_plot):
+        del layers, backdrop, things   # Unused.
 
-		# Apply motion commands.
-		if actions == 0:    # walk upward?
-			self._north(board, the_plot)
-		elif actions == 1:  # walk downward?
-			self._south(board, the_plot)
-		elif actions == 2:  # walk leftward?
-			self._west(board, the_plot)
-		elif actions == 3:  # walk rightward?
-			self._east(board, the_plot)
+        # Apply motion commands.
+        if actions == 0:    # walk upward?
+            self._north(board, the_plot)
+        elif actions == 1:  # walk downward?
+            self._south(board, the_plot)
+        elif actions == 2:  # walk leftward?
+            self._west(board, the_plot)
+        elif actions == 3:  # walk rightward?
+            self._east(board, the_plot)
 
-		# See if we've found the goal:
-		if self.position == (2, 1):
-			the_plot.add_reward(0.0)
-			the_plot.terminate_episode()
-			# print("Terminating episode..")
-			# time.sleep(10)
+        # See if we've found the goal:
+        if self.position == (2, 1):
+            the_plot.add_reward(0.0)
+            the_plot.terminate_episode()
+            # print("Terminating episode..")
+            # time.sleep(10)
 
-		else:
-			the_plot.add_reward(-1.0)
+        else:
+            the_plot.add_reward(-1.0)
+
 
 class PlayerSpriteSimpleR1(prefab_sprites.MazeWalker):
-	"""A `Sprite` for our player.
+    """A `Sprite` for our player.
 
-	This `Sprite` ties actions to going in the four cardinal directions. If we
-	reach a magical location (in this example, (4, 3)), the agent receives a
-	reward of 1 and the epsiode terminates.
-	"""
+    This `Sprite` ties actions to going in the four cardinal directions. If we
+    reach a magical location (in this example, (4, 3)), the agent receives a
+    reward of 1 and the epsiode terminates.
+    """
 
-	def __init__(self, corner, position, character):
-		"""Inform superclass that we can't walk through walls."""
-		super(PlayerSpriteSimpleR1, self).__init__(
-			corner, position, character, impassable='#')
+    def __init__(self, corner, position, character):
+        """Inform superclass that we can't walk through walls."""
+        super(PlayerSpriteSimpleR1, self).__init__(
+          corner, position, character, impassable='#')
 
-	def update(self, actions, board, layers, backdrop, things, the_plot):
-		del layers, backdrop, things   # Unused.
+    def update(self, actions, board, layers, backdrop, things, the_plot):
+        del layers, backdrop, things   # Unused.
 
-		# Apply motion commands.
-		if actions == 0:    # walk upward?
-			self._north(board, the_plot)
-		elif actions == 1:  # walk downward?
-			self._south(board, the_plot)
-		elif actions == 2:  # walk leftward?
-			self._west(board, the_plot)
-		elif actions == 3:  # walk rightward?
-			self._east(board, the_plot)
+        # Apply motion commands.
+        if actions == 0:    # walk upward?
+            self._north(board, the_plot)
+        elif actions == 1:  # walk downward?
+            self._south(board, the_plot)
+        elif actions == 2:  # walk leftward?
+            self._west(board, the_plot)
+        elif actions == 3:  # walk rightward?
+            self._east(board, the_plot)
 
-		# See if we've found the goal:
-		if self.position == (2, 2):
-			the_plot.add_reward(0.0)
-			the_plot.terminate_episode()
-			# print("Terminating episode..")
-			# time.sleep(10)
+        # See if we've found the goal:
+        if self.position == (2, 2):
+            the_plot.add_reward(0.0)
+            the_plot.terminate_episode()
+            # print("Terminating episode..")
+            # time.sleep(10)
 
-		else:
-			the_plot.add_reward(-1.0)
+        else:
+            the_plot.add_reward(-1.0)
 
 
-## Human control (for testing purposes):
+# Human control (for testing purposes):
 # def main(argv=()):
 # 	del argv  # Unused.
 #
--- sr_main.py
+++ sr_main.py
@@ -8,299 +8,299 @@ import matplotlib.pyplot as plt
 from collections import OrderedDict
 
 MAZE_R1 = ['#####',
- 		   '#P  #',
- 		   '#   #',
-		   '# G #',
-		   '#####']
+           '#P  #',
+           '#   #',
+           '# G #',
+           '#####']
 
 MAZE_R2 = ['#####',
- 		   '#P  #',
- 		   '#  G#',
-		   '#   #',
-		   '#####']
+           '#P  #',
+           '#  G#',
+           '#   #',
+           '#####']
 
 SIMPLE_MAZE_R1 = ['####',
-	 		      '#P #',
-			      '# G#',
-			      '####']
+                  '#P #',
+                  '# G#',
+                  '####']
 
 SIMPLE_MAZE_R2 = ['####',
-	 		      '#P #',
-			      '#G #',
-			      '####']
+                  '#P #',
+                  '#G #',
+                  '####']
+
 
 def flat2xy(f):
-	f = f + 1 # f indexes from 0, gridworld indexes from 1
-	y = f%ncol
-	x = (f-y)/ncol + 1
-	return x, y
+    f = f + 1  # f indexes from 0, gridworld indexes from 1
+    y = f % ncol
+    x = (f-y)/ncol + 1
+    return x, y
 
-def xy2flat(x,y):
-	f = (x-1)*9+y
-	f = f - 1 # f indexes from 0, gridworld indexes from 1
-	return f
+
+def xy2flat(x, y):
+    f = (x-1)*9+y
+    f = f - 1  # f indexes from 0, gridworld indexes from 1
+    return f
+
 
 # Take a random action
 def random_policy(rnd):
-	action = rnd.randint(0,4,1)[0]
-	return action
+    action = rnd.randint(0, 4, 1)[0]
+    return action
+
 
 def parse_obs(obs, nrow, ncol):
+    state_mtx = np.array(obs.layers['P'], dtype=np.float)
+    state_mtx = state_mtx[1:nrow+1, 1:ncol+1].flatten()
 
-	state_mtx = np.array(obs.layers['P'], dtype=np.float)
-	state_mtx = state_mtx[1:nrow+1,1:ncol+1].flatten()
-
-	return state_mtx.argmax()
+    return state_mtx.argmax()
 
 
 def setup_maze(maze_type, start_row, start_col):
 
-	if maze_type == 'SIMPLE_MAZE_R1':
-		maze_init = mazes.make_maze(SIMPLE_MAZE_R1, 'SIMPLE_MAZE_R1')
-		maze_update = mazes.make_maze(SIMPLE_MAZE_R2, 'SIMPLE_MAZE_R2')
-	elif maze_type == 'MAZE_R1':
-		maze_init = mazes.make_maze(MAZE_R1, 'MAZE_R1')
-		maze_update = mazes.make_maze(MAZE_R2, 'MAZE_R2')
+    if maze_type == 'SIMPLE_MAZE_R1':
+        maze_init = mazes.make_maze(SIMPLE_MAZE_R1, 'SIMPLE_MAZE_R1')
+        maze_update = mazes.make_maze(SIMPLE_MAZE_R2, 'SIMPLE_MAZE_R2')
+    elif maze_type == 'MAZE_R1':
+        maze_init = mazes.make_maze(MAZE_R1, 'MAZE_R1')
+        maze_update = mazes.make_maze(MAZE_R2, 'MAZE_R2')
 
-	# Place engines in play mode
-	maze_init.its_showtime()
-	maze_update.its_showtime()
+    # Place engines in play mode
+    maze_init.its_showtime()
+    maze_update.its_showtime()
 
-	# Move agent to starting position
-	maze_init._sprites_and_drapes['P']._teleport((start_row, start_col))
-	maze_update._sprites_and_drapes['P']._teleport((start_row, start_col))
+    # Move agent to starting position
+    maze_init._sprites_and_drapes['P']._teleport((start_row, start_col))
+    maze_update._sprites_and_drapes['P']._teleport((start_row, start_col))
 
-	return maze_init, maze_update
+    return maze_init, maze_update
+
 
 # Indicator Function
 def indicator(s, j):
-	if s == j:
-		return 1
-	else:
-		return 0
+    if s == j:
+        return 1
+    else:
+        return 0
+
 
 # Track2 Q1
 def run_learning_sr(config):
 
-	# Initializations
-	maze_type = config['maze_type']
-	terminal_step = config['terminal_step']
+    # Initializations
+    maze_type = config['maze_type']
+    terminal_step = config['terminal_step']
 
-	episode_len = config['episode_length']
-	nrow = config['maze_params']['row']
-	ncol = config['maze_params']['col']
-	start_row = config['maze_params']['start_row']
-	start_col = config['maze_params']['start_col']
+    episode_len = config['episode_length']
+    nrow = config['maze_params']['row']
+    ncol = config['maze_params']['col']
+    start_row = config['maze_params']['start_row']
+    start_col = config['maze_params']['start_col']
 
-	alpha = config['learning_alg_params']['alpha']
-	gamma = config['learning_alg_params']['gamma']
+    alpha = config['learning_alg_params']['alpha']
+    gamma = config['learning_alg_params']['gamma']
 
-	state_len = nrow*ncol
-	action_len = 4
-	reward_len = 2
+    state_len = nrow*ncol
+    action_len = 4
+    reward_len = 2
 
-	# Don't need all these guys
-	rnd = np.random.RandomState(24)
+    # Don't need all these guys
+    rnd = np.random.RandomState(24)
 
-	S = xy2flat(start_row, start_col)
-	S_prime = S
+    S = xy2flat(start_row, start_col)
+    S_prime = S
 
-	# Initialize mazes (r_1, r_2) and agent at starting position
-	maze_init, _ = setup_maze(maze_type, start_row, start_col)
-	curr_maze = maze_init
+    # Initialize mazes (r_1, r_2) and agent at starting position
+    maze_init, _ = setup_maze(maze_type, start_row, start_col)
+    curr_maze = maze_init
 
-	step = 0
-	episode = 0
-	episode_step = 0
-	result = OrderedDict()
+    step = 0
+    episode = 0
+    episode_step = 0
+    result = OrderedDict()
 
-	cum_reward = 0
-	cum_reward_lst = []
+    cum_reward = 0
+    cum_reward_lst = []
 
-	Phi_pi = np.zeros((state_len, state_len))
-	V_pi = np.zeros((state_len,1))
-	result['config'] = config
+    Phi_pi = np.zeros((state_len, state_len))
+    V_pi = np.zeros((state_len,1))
+    result['config'] = config
 
+    while step < terminal_step:
 
-	while step < terminal_step:
+        # Reset episode:
+        if episode_step >= episode_len:
+            S = xy2flat(start_row, start_col)
+            S_prime = S
+            maze_init, _ = setup_maze(maze_type, start_row, start_col)
+            curr_maze = maze_init # only doing this to reset the agent to the starting position, the next if statement will actually correct the map if need be
 
-		# Reset episode:
-		if episode_step >= episode_len:
-			S = xy2flat(start_row, start_col)
-			S_prime = S
-			maze_init, _ = setup_maze(maze_type, start_row, start_col)
-			curr_maze = maze_init # only doing this to reset the agent to the starting position, the next if statement will actually correct the map if need be
+            episode_step = 0
+            episode += 1
 
-			episode_step = 0
-			episode += 1
+        if curr_maze._game_over:
+            A = random_policy(rnd)
+            R = 0.
+            S_prime = S
+        else:
+            # Select Action A using a uniform random policy
+            A = random_policy(rnd)
 
-		if curr_maze._game_over:
-			A = random_policy(rnd)
-			R = 0.
-			S_prime = S
-		else:
-			# Select Action A using a uniform random policy
-			A = random_policy(rnd)
+            # Apply action A to current maze, get reward R, and new state S'
+            obs, R, _ = curr_maze.play(A)
+            S_prime = parse_obs(obs, nrow, ncol)
 
-			# Apply action A to current maze, get reward R, and new state S'
-			obs, R, _ = curr_maze.play(A)
-			S_prime = parse_obs(obs, nrow, ncol)
+        for j in range(state_len):
+            Phi_pi[S, j] = Phi_pi[S, j] + alpha*(indicator(S,j) + gamma*Phi_pi[S_prime, j] - Phi_pi[S, j])
+            V_pi[S] = V_pi[S] + alpha*(R + gamma*V_pi[S_prime] - V_pi[S])
 
-		for j in range(state_len):
-			Phi_pi[S, j] = Phi_pi[S, j] + alpha*(indicator(S,j) + gamma*Phi_pi[S_prime, j] - Phi_pi[S, j])
-			V_pi[S] = V_pi[S] + alpha*(R + gamma*V_pi[S_prime] - V_pi[S])
+        experience = {'S': S,
+                      'A': A,
+                      'R': R,
+                      'S_prime': S_prime
+                      }
 
-		experience = {
-			'S' : S,
-			'A' : A,
-			'R' : R,
-			'S_prime' : S_prime
-		}
-		result[step] = {
-			'Phi_pi' : Phi_pi.copy(),
-			'V_pi' : V_pi.copy(),
-			'experience': experience
+        result[step] = {'Phi_pi': Phi_pi.copy(),
+                        'V_pi': V_pi.copy(),
+                        'experience': experience
+                        }
 
-		}
+        # Update to new state
+        S = S_prime
 
-		# Update to new state
-		S = S_prime
+        step += 1
+        episode_step += 1
 
-		step += 1
-		episode_step += 1
+    return result
 
-	return result
 
 def run_learning_sr_switch(config):
 
-	# Initializations
-	maze_type = config['maze_type']
-	terminal_step = config['terminal_step']
-	switch_step = config['switch_reward_at_step']
+    # Initializations
+    maze_type = config['maze_type']
+    terminal_step = config['terminal_step']
+    switch_step = config['switch_reward_at_step']
 
-	episode_len = config['episode_length']
-	nrow = config['maze_params']['row']
-	ncol = config['maze_params']['col']
-	start_row = config['maze_params']['start_row']
-	start_col = config['maze_params']['start_col']
+    episode_len = config['episode_length']
+    nrow = config['maze_params']['row']
+    ncol = config['maze_params']['col']
+    start_row = config['maze_params']['start_row']
+    start_col = config['maze_params']['start_col']
 
-	alpha = config['learning_alg_params']['alpha']
-	gamma = config['learning_alg_params']['gamma']
+    alpha = config['learning_alg_params']['alpha']
+    gamma = config['learning_alg_params']['gamma']
 
-	state_len = nrow*ncol
-	action_len = 4
-	reward_len = 2
+    state_len = nrow*ncol
+    action_len = 4
+    reward_len = 2
 
-	# Don't need all these guys
-	rnd = np.random.RandomState(24)
+    # Don't need all these guys
+    rnd = np.random.RandomState(24)
 
-	S = xy2flat(start_row, start_col)
-	S_prime = S
+    S = xy2flat(start_row, start_col)
+    S_prime = S
 
-	# Initialize mazes (r_1, r_2) and agent at starting position
-	maze_init, maze_update = setup_maze(maze_type, start_row, start_col)
-	curr_maze = maze_init
+    # Initialize mazes (r_1, r_2) and agent at starting position
+    maze_init, maze_update = setup_maze(maze_type, start_row, start_col)
+    curr_maze = maze_init
 
-	step = 0
-	episode = 0
-	episode_step = 0
-	result = OrderedDict()
+    step = 0
+    episode = 0
+    episode_step = 0
+    result = OrderedDict()
 
-	cum_reward = 0
-	cum_reward_lst = []
+    cum_reward = 0
+    cum_reward_lst = []
 
-	Phi_pi = np.zeros((state_len, state_len))
-	V_pi = np.zeros((state_len,1))
-	result['config'] = config
+    Phi_pi = np.zeros((state_len, state_len))
+    V_pi = np.zeros((state_len,1))
+    result['config'] = config
 
+    while step < terminal_step:
+        # Reset episode:
+        if episode_step >= episode_len:
+            S = xy2flat(start_row, start_col)
+            S_prime = S
+            maze_init, maze_update = setup_maze(maze_type, start_row, start_col)
+            curr_maze = maze_init # only doing this to reset the agent to the starting position, the next if statement will actually correct the map if need be
 
-	while step < terminal_step:
+            episode_step = 0
+            episode += 1
 
-		# Reset episode:
-		if episode_step >= episode_len:
-			S = xy2flat(start_row, start_col)
-			S_prime = S
-			maze_init, maze_update = setup_maze(maze_type, start_row, start_col)
-			curr_maze = maze_init # only doing this to reset the agent to the starting position, the next if statement will actually correct the map if need be
+        # The maze evolves after switch_step:
+        if step <= switch_step:
+            curr_maze = maze_init
+        else:
+            old_row, old_col = curr_maze._sprites_and_drapes['P']._virtual_row, \
+                               curr_maze._sprites_and_drapes['P']._virtual_col
 
-			episode_step = 0
-			episode += 1
+            maze_update._sprites_and_drapes['P']._teleport((old_row, old_col))
+            curr_maze = maze_update
 
-		# The maze evolves after switch_step:
-		if step <= switch_step:
-			curr_maze = maze_init
-		else:
-			old_row, old_col = curr_maze._sprites_and_drapes['P']._virtual_row, \
-							   curr_maze._sprites_and_drapes['P']._virtual_col
+        if curr_maze._game_over:
+            A = random_policy(rnd)
+            R = 0.
+            S_prime = S
+        else:
+            # Select Action A using a uniform random policy
+            A = random_policy(rnd)
 
-			maze_update._sprites_and_drapes['P']._teleport((old_row, old_col))
-			curr_maze = maze_update
+            # Apply action A to current maze, get reward R, and new state S'
+            obs, R, _ = curr_maze.play(A)
+            S_prime = parse_obs(obs, nrow, ncol)
 
-		if curr_maze._game_over:
-			A = random_policy(rnd)
-			R = 0.
-			S_prime = S
-		else:
-			# Select Action A using a uniform random policy
-			A = random_policy(rnd)
+        for j in range(state_len):
+            Phi_pi[S, j] = Phi_pi[S, j] + alpha*(indicator(S,j) + gamma*Phi_pi[S_prime, j] - Phi_pi[S, j])
+            V_pi[S] = V_pi[S] + alpha*(R + gamma*V_pi[S_prime] - V_pi[S])
 
-			# Apply action A to current maze, get reward R, and new state S'
-			obs, R, _ = curr_maze.play(A)
-			S_prime = parse_obs(obs, nrow, ncol)
+        experience = {'S': S,
+                      'A': A,
+                      'R': R,
+                      'S_prime': S_prime
+                      }
 
-		for j in range(state_len):
-			Phi_pi[S, j] = Phi_pi[S, j] + alpha*(indicator(S,j) + gamma*Phi_pi[S_prime, j] - Phi_pi[S, j])
-			V_pi[S] = V_pi[S] + alpha*(R + gamma*V_pi[S_prime] - V_pi[S])
+        result[step] = {'Phi_pi' : Phi_pi.copy(),
+                        'V_pi' : V_pi.copy(),
+                        'experience': experience
+                        }
 
-		experience = {
-			'S' : S,
-			'A' : A,
-			'R' : R,
-			'S_prime' : S_prime
-		}
-		result[step] = {
-			'Phi_pi' : Phi_pi.copy(),
-			'V_pi' : V_pi.copy(),
-			'experience': experience
+        # Update to new state
+        S = S_prime
 
-		}
+        step += 1
+        episode_step += 1
 
-		# Update to new state
-		S = S_prime
-
-		step += 1
-		episode_step += 1
-
-	return result
-
+    return result
 
 
 def main(argv):
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('config_file', type=str,  help="Configuration file path. e.g. blocking.config")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config_file', type=str,  help="Configuration file path. e.g. blocking.config")
+    args = parser.parse_args()
 
-	#Reading config file
-	config_file_path = args.config_file
+    # Reading config file
+    config_file_path = args.config_file
 
-	if not os.path.exists(config_file_path): raise argparse.ArgumentTypeError('Not a valid config file')
+    if not os.path.exists(config_file_path):
+        raise argparse.ArgumentTypeError('Not a valid config file')
 
-	with open(config_file_path, 'r') as config_fd:
-		config = json.load(config_fd)
+    with open(config_file_path, 'r') as config_fd:
+        config = json.load(config_fd)
 
-	result = run_learning_sr(config)
-	# cum_reward = 0
-	# cum_reward_lst = []
-	# for i in range(len(result)):
-	# 	cum_reward += result[i]['experience']['R']
-	# 	cum_reward_lst.append(cum_reward)
-	#
-	# print("Cumulative reward: " + str(cum_reward_lst[-1]))
-	# plt.plot(cum_reward_lst)
-	# plt.ylabel('Cumulative Rewards')
-	# plt.xlabel('Step number')
-	# plt.show()
+    result = run_learning_sr(config)
+    # cum_reward = 0
+    # cum_reward_lst = []
+    # for i in range(len(result)):
+    # 	cum_reward += result[i]['experience']['R']
+    # 	cum_reward_lst.append(cum_reward)
+    #
+    # print("Cumulative reward: " + str(cum_reward_lst[-1]))
+    # plt.plot(cum_reward_lst)
+    # plt.ylabel('Cumulative Rewards')
+    # plt.xlabel('Step number')
+    # plt.show()
+
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
